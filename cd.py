17766538813083f8a1c8dfede5f662c0220fa3f7
Nova <novamoon1@gmail.com>
2015-12-26 21:47:49 +0100

    Update cd.py

--- cd.py
+++ cd.py
@@ -1,16 +1,2 @@
-cdtest = 0
 cduptime = 0
-cdcommands = 0
-cdgarbo = 0
-cdtagface = 0
-cdfaq = 0
-cdmemes = 0
-cdkirby = 0
-cduuptime = 0
-cdimportant = 0
 cdwr = 0
-cdcandy = 0
-cdsellout = 0
-cdkeyboard = 0
-cdwannabes = 0
-cd = 0
